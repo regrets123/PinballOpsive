9b304581b41949b99f806da79065b29c11c535e2
Daniel Saxe <danielsaxe@gmail.com>
2022-10-01 16:57:06 +0200

    merged ghost lady to world

--- Assets/InputSystem/TestingInputSystem.cs
+++ Assets/InputSystem/TestingInputSystem.cs
@@ -7,14 +7,12 @@ public class TestingInputSystem : MonoBehaviour
 {
     [SerializeField]
     private float speed = 0.1f;
-    Rigidbody characterRigidBody;
     PinballOpsiveInputSystem playerInputActions;
     private Animator animator;
 
 
     private void Awake()
     {
-        characterRigidBody = GetComponent<Rigidbody>();
         animator = GetComponent<Animator>();
 
         playerInputActions = new PinballOpsiveInputSystem();
@@ -25,8 +23,6 @@ public class TestingInputSystem : MonoBehaviour
     private void FixedUpdate()
     {
         Vector2 inputVector = playerInputActions.Player.Move.ReadValue<Vector2>();
-        Vector3 newPosition = characterRigidBody.position + new Vector3(inputVector.x, 0, inputVector.y) * speed;
-        characterRigidBody.MovePosition(newPosition);
         animator.SetFloat("sideMovement", inputVector.x, .1f, Time.deltaTime);
         animator.SetFloat("forwardMovement", inputVector.y, .1f, Time.deltaTime);
     }
@@ -45,7 +41,6 @@ public class TestingInputSystem : MonoBehaviour
         if (context.performed)
         {
             Debug.Log("Jump " + context.phase);
-            characterRigidBody.AddForce(Vector3.up * 5f, ForceMode.Impulse);
         }
     }
 }
